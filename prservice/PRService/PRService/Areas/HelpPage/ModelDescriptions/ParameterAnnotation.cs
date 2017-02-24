211e4c21859ced835959bcd13b7c35f286e59ce3
詹華 <zhh9113@hotmail.com>
2017-02-24 16:20:29 +0800

    init prservice

---
+++ prservice/PRService/PRService/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PRService.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
